76ae6316037166f2fd03060a41dcf68a5be2e213
Fulvio Fanelli <70142915+fulviofanelli-unoeste@users.noreply.github.com>
2025-03-19 17:10:56 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const outputJson = require("./swagger-output.json");
+import swaggerUi from 'swagger-ui-express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+//configura as ferramentas de parser
+app.use(cookieParser());
+app.use(express.json());
+
+//página de documentação
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(outputJson));
+
+app.listen(5000, function() {
+    console.log("backend em execução");
+})
---
+++ swagger.js
@@ -0,0 +1,23 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+    info: {
+        title: "API - Unileste",
+        description: "API utilizada para o serviços digitais da universidade"
+    },
+    host: 'localhost:5000',
+    components: {
+        securitySchemes:{
+        },
+        schemas: {
+        },
+    }
+}
+
+const outputJson = "./swagger-output.json";
+const routes = []
+
+swaggerAutogen({openapi: '3.0.0'})(outputJson, routes, doc)
+.then( async () => {
+    await import('./server.js');
+})
